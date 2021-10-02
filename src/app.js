8f0da2742a85d4c9cdbe68984272c6f7fe24946d
santosh325 <santoshthapa.ts12@gmail.com>
2021-10-02 15:26:01 +0545

    inserting mulitple document in mongo db

--- src/app.js
+++ src/app.js
@@ -14,3 +14,39 @@ const playListSchema = new mongoose.Schema({
 });
 
 const PlayList = new mongoose.model("PlayList", playListSchema);
+
+
+const createDocument = async() => {
+
+  try {
+    const santosh = new PlayList({
+      name:'Santosh',
+      address: 'Butwal',
+      isActive: true,
+    
+    })
+    const messi = PlayList({
+      name: 'messi',
+      address: 'Argentina',
+      isActive: true,
+    })
+    const mandip = PlayList({
+      name: "mandip",
+      address:"pokhara",
+      isActive: false,
+    })
+    const ronaldo = PlayList({
+      name: "ronaldo",
+      address:"portugal",
+      isActive: true,
+    })
+
+    const result = await PlayList.insertMany([santosh,messi,mandip,ronaldo])
+    console.log(result);
+  }catch(e) {
+    console.log(e);
+  }
+ 
+}
+
+createDocument();
